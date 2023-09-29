b6206fca8556a643a702ca1b598ce63e9c110f37
Paul Byrne <paulgregorybyrne@gmail.com>
2023-09-29 16:45:47 +0200

    Get the windows aligned properly - Full width when there is one, half-width when two

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import React, { useState } from 'react';
 import Image from 'next/image'
 
 export default function Home() {
-  const [window1Visible, setWindow1Visible] = useState(false);
-  const [window2Visible, setWindow2Visible] = useState(false);
+  const [window1Visible, setWindow1Visible] = useState(true);
+  const [window2Visible, setWindow2Visible] = useState(true);
   const [bothWindowsHidden, setBothWindowsHidden] = useState(true);
 
   const toggleWindow1 = () => {
@@ -22,27 +22,31 @@ export default function Home() {
   };
 
   return (
-    <div className={`App ${bothWindowsHidden ? 'full-width' : ''}`}>
-      <button onClick={toggleWindow1}>
-        {window1Visible ? 'Hide Window 1' : 'Show Window 1'}
-      </button>
-      <button onClick={toggleWindow2}>
-        {window2Visible ? 'Hide Window 2' : 'Show Window 2'}
-      </button>
+    <div className={`App`}>
+      <div className="menu">
+        <button className="button" onClick={toggleWindow1}>
+          {window1Visible ? 'Hide Window 1' : 'Show Window 1'}
+        </button>
+        <button className="button" onClick={toggleWindow2}>
+          {window2Visible ? 'Hide Window 2' : 'Show Window 2'}
+        </button>
+      </div>
 
-      {window1Visible && (
-        <div className="window" id="window1">
-          <h2>Window 1</h2>
-          {/* Content for Window 1 */}
-        </div>
-      )}
+      <div className="windows">
+        {window1Visible && (
+          <div className="window" id="window1">
+            <h2>Window 1</h2>
+            {/* Content for Window 1 */}
+          </div>
+        )}
 
-      {window2Visible && (
-        <div className="window" id="window2">
-          <h2>Window 2</h2>
-          {/* Content for Window 2 */}
-        </div>
-      )}
+        {window2Visible && (
+          <div className="window" id="window2">
+            <h2>Window 2</h2>
+            {/* Content for Window 2 */}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
